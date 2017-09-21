f96091fe7005167350d3aa44c89a567a8e2b1f29
seedy.dupuis <seedy.dupuis@gmail.com>
2017-09-21 04:21:43 +0200

    feat : favorite btn component + new attr for marvel char

--- src/app/shared/marvel-character/marvel-character.ts
+++ src/app/shared/marvel-character/marvel-character.ts
@@ -1,4 +1,5 @@
 import {MarvelComic} from '../marvel-comic/marvel-comic';
+
 export class MarvelCharacter {
   public comics: MarvelComic[];
   public img: {small: string, medium: string, xlarge: string};
@@ -7,8 +8,9 @@ export class MarvelCharacter {
     public name: string,
     public description: string,
     private imgItem: {path: string, extension: string},
-    private comicItems: any
-  ) {
+    private comicItems: any,
+    public favorite = false
+) {
     this.img = {
       small: imgItem.path + '/portrait_small.' + imgItem.extension,
       medium: imgItem.path + '/portrait_medium.' + imgItem.extension,
